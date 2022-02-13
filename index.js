f4b9016973790908647535c59348c4b0931db317
Aaron Chris <rooneychris83@gmail.com>
2022-02-13 02:07:47 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let count = 0
+let countEL = document.getElementById("count-el")
+let saveEL  = document.getElementById("save-el")
+
+
+function increment() {
+    count += 1
+
+countEL.innerText = count
+
+}
+function save() {
+  console.log(count);
+
+
+  let showcase = count + "  -  "
+
+  saveEL.innerText += showcase
+}
